88b04282e658afb161482dd668e2d9919bb5e22d
brwnalima <112510971+brwnalima@users.noreply.github.com>
2022-11-16 17:22:31 -0300

    up
    efeito slide nos cards

--- language-plus/src/components/CardItem.jsx
+++ language-plus/src/components/CardItem.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 // usando props pra mudar a propriedade no Cards.jsx
--- language-plus/src/components/Cards.jsx
+++ language-plus/src/components/Cards.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
-import './Cards.css';
-import CardItem from './CardItem';
+import React from 'react'
+import './Cards.css'
+import CardItem from './CardItem'
+import { Slide } from "react-awesome-reveal"
+
+
 
 function Cards() {
+
     return (
+
         <div className='cards'>
-            <h1>Lorem ipsum dolor sit amet, consectetur!</h1>
-            <div className='cards__container'>
-                <div className='cards__wrapper'>
-                    <ul className='cards__items'>
-                        <CardItem
-                            //substituindo os props
-                            src='images/aula-remota.jpg'
-                            text='Inscreva-se já em nossos cursos on-lines e assista suas aulas onde preferir.'
-                            label='AULAS REMOTAS'
-                            path='/cursos'
-                        />
-                        <CardItem
-                            src='images/aula-presencial.jpg'
-                            text='Você pode também, estar mais próximo ao seus colegas e professores! Faça cada vez mais network durante as aulas.'
-                            label='AULAS PRESENCIAIS'
-                            path='/cursos'
-                        />
-                    </ul>
+            <Slide cascade damping={0.1}>
+                <h1>Lorem ipsum dolor sit amet, consectetur!</h1>
+            </Slide>
+            <Slide cascade damping={0.1}>
+                <div className='cards__container'>
+                    <div className='cards__wrapper'>
+                        <ul className='cards__items'>
+                            <CardItem
+                                //substituindo os props
+                                src='images/aula-remota.jpg'
+                                text='Inscreva-se já em nossos cursos on-lines e assista suas aulas onde preferir.'
+                                label='AULAS REMOTAS'
+                                path='/cursos'
+                            />
+                            <CardItem
+                                src='images/aula-presencial.jpg'
+                                text='Você pode também, estar mais próximo ao seus colegas e professores! Faça cada vez mais network durante as aulas.'
+                                label='AULAS PRESENCIAIS'
+                                path='/cursos'
+                            />
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </Slide>
         </div>
     );
 }
