dc257512ae24edd396c111f72aca250db3b5388f
suannyrodrigues <suannyrsantos@gmail.com>
2022-11-21 11:52:41 -0300

    Atualização na página Sobre Nós
    Atualizações na página Sobre Nós.

--- language-plus/src/components/conteudo/Sobrenos.jsx
+++ language-plus/src/components/conteudo/Sobrenos.jsx
@@ -8,7 +8,7 @@ export default function Sobrenos() {
        <h1>Você sonha,</h1>
        <h1>A gente realiza.</h1>
        </div>
-       <div>
+       <div className="conteudo">
         <h2>Um experiência única para você aprender um novo idioma</h2>
         <h3>Comunicação e Aprendizagem</h3>
         <p>Na Language+, desde a primeira aula o aluno já se comunica na língua que está aprendendo. Ele trabalha a comunicação oral e escrita em atividades que simulam situações do dia a dia. É na interação que o aluno aprende o idioma, e não repetindo e memorizando frases. Assim o curso torna-se dinâmico, divertido e eficiente. A gramática também é trabalhada de forma natural.</p>
@@ -16,6 +16,5 @@ export default function Sobrenos() {
         <p>Nossa missão é habilitar nossos alunos a se comunicar, se desenvolver pessoal e profissionalmente dentro de um tempo adequado, produtivo e divertido.</p>
      </div>
   </div>
-    )
-  
+    ) 
   }
