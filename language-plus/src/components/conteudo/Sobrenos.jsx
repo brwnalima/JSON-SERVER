e6bde9a132e9778d5397000af5eb18184f1bc9c4
brwnalima <112510971+brwnalima@users.noreply.github.com>
2022-11-21 22:58:27 -0300

    sobre nos

--- language-plus/src/components/conteudo/Sobrenos.jsx
+++ language-plus/src/components/conteudo/Sobrenos.jsx
@@ -4,30 +4,32 @@ import '../conteudo/Sobrenos.css'
 function Sobrenos() {
     return (
         <div className="conteudo">
-            <div className="tudo">
-                <h1 className="titulo">Não existe hora ou lugar certo, busque sempre o conhecimento!</h1>
-                <h4>Aprenda com professores nativos e certificados, quando e onde você quiser</h4>
-                <div className="container">
-                    <div className="row">
-                        <div class="conteiner-textos">
-                            <div>
-                                <img id="foto-mala" src="https://cdn-icons-png.flaticon.com/512/74/74468.png" className="icone" />
-                                <h3>Os cursos de idiomas da Language Plus são focados em seu objetivo!</h3>
-                                <p>Somos uma empresa que busca sempre a satisfação dos nossos clientes, fazendo com que a inovação e o aprendizado andem juntos! Se o seu desejo é viajar, conhecer os lugares para onde pretende ir, aprender sobre os pontos turísticos, vocabulário local, sobre transportes e hospedagem. Sinta-se seguro para estar em um novo lugar e aprender ainda mais sobre outras culturas!</p>
-                            </div>
-                            <div>
-                                <img src="https://cdn-icons-png.flaticon.com/512/662/662345.png" className="icone" />
-                                <h3>Se o seu objetivo é melhorar o seu currículo e crescer na sua carreira, aprender um novo idioma focando no seu desempenho profissional.</h3>
-                                <p>Ter uma comunicação clara em um ambiente de negócios, desde o atendimento de ligações a respostas de e-mails, bem como na criação de relatórios e rotinas administrativas, saber uma língua estrangeira é fundamental para quem almeja uma carreira internacional ou trabalhar em uma empresa multinacional.</p>
-                            </div>
-                            <div>
-                                <img src="https://cdn-icons-png.flaticon.com/512/84/84216.png" className="icone" />
-                                <h3>Se você é um programador e precisa enriquecer seu vocabulário de inglês para programação, estudar conosco é opção perfeita para você.</h3>
-                                <p>No nosso curso você aprenderá o vocabulário de forma prática e divertida para desenvolver e se desenvolver cada vez melhor! Começando com a introdução aos fundamentos do idioma, vocabulários básicos e conversação do dia a dia, temas comuns como escola e esportes, expressões e frases simples, apresente sua criança a um mundo novo cheio de oportunidades! Com o avanço do curso seu filho aperfeiçoará ainda mais a pronúncia e sua autoconfiança!</p>
-                            </div>
-                        </div>
+            <div id="tudo">
+
+                <div className="titulo">
+                    <h1>Não existe hora ou lugar certo, busque sempre o conhecimento!</h1>
+                    <h4>Aprenda com professores nativos e certificados, quando e onde você quiser</h4>
+                </div>
+
+
+                <div class="conteiner-textos">
+                    <div>
+                        <img id="foto-mala" src="https://cdn-icons-png.flaticon.com/512/74/74468.png" className="icone" />
+                        <h3>Os cursos de idiomas da Language Plus são focados em seu objetivo!</h3>
+                        <p>Somos uma empresa que busca sempre a satisfação dos nossos clientes, fazendo com que a inovação e o aprendizado andem juntos! Se o seu desejo é viajar, conhecer os lugares para onde pretende ir, aprender sobre os pontos turísticos, vocabulário local, sobre transportes e hospedagem. Sinta-se seguro para estar em um novo lugar e aprender ainda mais sobre outras culturas!</p>
+                    </div>
+                    <div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/662/662345.png" className="icone" />
+                        <h3>Se o seu objetivo é melhorar o seu currículo e crescer na sua carreira, aprender um novo idioma focando no seu desempenho profissional.</h3>
+                        <p>Ter uma comunicação clara em um ambiente de negócios, desde o atendimento de ligações a respostas de e-mails, bem como na criação de relatórios e rotinas administrativas, saber uma língua estrangeira é fundamental para quem almeja uma carreira internacional ou trabalhar em uma empresa multinacional.</p>
+                    </div>
+                    <div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/84/84216.png" className="icone" />
+                        <h3>Se você é um programador e precisa enriquecer seu vocabulário de inglês para programação, estudar conosco é opção perfeita para você.</h3>
+                        <p>No nosso curso você aprenderá o vocabulário de forma prática e divertida para desenvolver e se desenvolver cada vez melhor! Começando com a introdução aos fundamentos do idioma, vocabulários básicos e conversação do dia a dia, temas comuns como escola e esportes, expressões e frases simples, apresente sua criança a um mundo novo cheio de oportunidades! Com o avanço do curso seu filho aperfeiçoará ainda mais a pronúncia e sua autoconfiança!</p>
                     </div>
                 </div>
+
             </div>
         </div>
     )
