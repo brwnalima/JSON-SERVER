8549e4577306b904b554cf72ce10905edb08a462
brwnalima <bruna.lima@edu.unirio.br>
2023-02-01 19:50:28 -0300

    atualizaçãoooo

--- language-plus/src/components/cards/Cards.jsx
+++ language-plus/src/components/cards/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Cards.css'
 import CardItem from './CardItem';
-import { Slide } from "react-awesome-reveal"
+import { Slide } from "'react-awesome-reveal'"
 
 
 
